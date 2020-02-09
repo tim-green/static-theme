58c9d2458411cc1fc7f378b3758adc5ef175e344
Tim <github@timgreen.ws>
2020-02-09 19:58:19 +1100

    template for displaying the footer.

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* The template for displaying the footer.
+*
+* Contains the closing of the #content div and all content after
+*
+* @package static
+* @since 1.0
+*/
+?> 
+    <?php static_get_template_part('footer'); ?>
+    <?php wp_footer(); ?>
+</body>
+</html>
