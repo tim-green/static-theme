9496a0f93ee076a7655a89fe88ad2e0611461ff3
Tim <github@timgreen.ws>
2020-02-09 19:05:04 +1100

    Translated text & escapes it for safe use for output

--- inc/classes/static.php
+++ inc/classes/static.php
@@ -57,3 +57,42 @@ class static_Static {
     }
 
     /**
+     * @since v1.0
+     */
+    public static function total_grid() {
+        return array(
+            '1' => esc_html__( '1 Grid', 'static' ),
+            '2' => esc_html__( '2 Grid', 'static' ),
+            '3' => esc_html__( '3 Grid', 'static' ),
+            '4' => esc_html__( '4 Grid', 'static' ),
+            '5' => esc_html__( '5 Grid', 'static' ),
+            '6' => esc_html__( '6 Grid', 'static' ),
+            '7' => esc_html__( '7 Grid', 'static' ),
+            '8' => esc_html__( '8 Grid', 'static' ),
+            '9' => esc_html__( '9 Grid', 'static' ),
+            '10' => esc_html__( '10 Grid', 'static' ),
+            '11' => esc_html__( '11 Grid', 'static' ),
+            '12' => esc_html__( '12 Grid', 'static' ),
+        );
+    }
+
+    /**
+     * @since v1.0
+     */
+    public static function total_items() {
+        return array(
+            '2' => esc_html__( 'Two', 'static' ),
+            '3' => esc_html__( 'Three', 'static' ),
+            '4' => esc_html__( 'Four', 'static' ),
+            '5' => esc_html__( 'Five', 'static' ),
+            '6' => esc_html__( 'Six', 'static' ),
+            '7' => esc_html__( 'Seven', 'static' ),
+        );
+    }
+
+}
+
+// Removing this line is like not having a functions.php file
+new static_Static;
+
+endif;
