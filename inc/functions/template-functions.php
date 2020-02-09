39645e9e35eb22009a8ba323a1510a3749c0ddfc
Tim <github@timgreen.ws>
2020-02-09 19:55:30 +1100

    add the preloader for site

--- inc/functions/template-functions.php
+++ inc/functions/template-functions.php
@@ -1,3 +1,18 @@
+if ( ! function_exists( 'static_preloader' ) ) :
+    function static_preloader() {  
+        if( static_get_options('preloader') ) { ?>
+        <div class="preloader">
+            <div class="preloader-inner">
+                <div class="preloader-icon">
+                    <span></span>
+                    <span></span>
+                </div><!-- /preloader-icon -->
+            </div><!-- /preloader-inner -->
+        </div><!-- /preloader -->
+        <?php }
+    }
+endif;
+
 /**
  *  Get theme options
  *
