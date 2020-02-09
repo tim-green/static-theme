1db083c91486cf9e3b6d760744de620c254d19b4
Tim <github@timgreen.ws>
2020-02-09 19:02:40 +1100

    Allow HTML tag from escaping HTML

---
+++ inc/classes/static.php
@@ -0,0 +1,59 @@
+<?php 
+if ( ! defined( 'ABSPATH' ) ) die( esc_html__( 'Direct access forbidden.', 'static' ) );
+
+if( ! class_exists( 'static_Static' ) ) :
+class static_Static {
+
+    /**
+     * Allow HTML tag from escaping HTML 
+     * 
+     * @return void
+     * @since v1.0
+     */
+    public static function html_allow() {
+        return array(
+            'a' => array(
+                'href' => array(),
+                'title' => array()
+            ),
+            'br' => array(),
+            'del' => array(),
+            'span' => array(),
+            'em' => array(),
+            'strong' => array(),
+            'h1' => array(
+                'class' => array(),
+                'id' => array(),
+            ),            
+            'h2' => array(
+                'class' => array(),
+                'id' => array(),
+            ),            
+            'h3' => array(
+                'class' => array(),
+                'id' => array(),
+            ),            
+            'h4' => array(
+                'class' => array(),
+                'id' => array(),
+            ),            
+            'h5' => array(
+                'class' => array(),
+                'id' => array(),
+            ),            
+            'h6' => array(
+                'class' => array(),
+                'id' => array(),
+            ),            
+            'div' => array(
+                'class' => array(),
+                'id' => array(),
+            ),
+            'p' => array(
+                'class' => array(),
+                'id' => array(),
+            ),
+        );
+    }
+
+    /**
